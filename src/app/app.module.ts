7e60e24b45daaf7257e9234512f51d3d37d176c2
Carlos Tiago <carlostiagodesousa@gmail.com>
2019-12-23 00:30:10 -0300

    inserindo primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {TabViewModule} from 'primeng/tabview';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
